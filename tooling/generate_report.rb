ec98ae792f9d1ec71d622489c3988d54c5143f59
Connor Stack <sirconnorstack@gmail.com>
2022-04-15 17:38:23 -0400

    Fill out the report.

--- tooling/generate_report.rb
+++ tooling/generate_report.rb
@@ -1,5 +1,30 @@
 require_relative './song_list.rb'
+require 'byebug'
+
+def histogram(values)
+  values.group_by { |value| value }.map { |value, array| [value, array.count] }
+end
+
+def print_histogram(values)
+  histogram(values).sort_by(&:last).each do |value, count|
+    puts "#{value}: #{count}"
+  end
+end
 
 filepath = File.expand_path(File.join(File.dirname(__FILE__), '../data/songs.csv'))
 song_list = SongList.new(filepath)
 
+puts "By Decade"
+print_histogram(song_list.songs.map(&:decade))
+
+puts "By Duration"
+print_histogram(song_list.songs.map(&:duration_minutes))
+
+puts "By Number of Singers"
+print_histogram(song_list.songs.map(&:number_of_singers))
+
+puts "Combined"
+print_histogram(song_list.songs.map do |song|
+  type = song.number_of_singers == 1 ? "Solo" : "Duet"
+  "#{song.decade}-#{type}"
+end)
--- tooling/song_list.rb
+++ tooling/song_list.rb
@@ -2,26 +2,75 @@ require 'csv'
 require 'byebug'
 
 class SongList
+  class Song
+    class << self
+      def from_row(row)
+        new(
+          title: row["title"],
+          artist: row["artist"],
+          number_of_singers: row["number_of_singers"].to_i,
+          year: row["year"].to_i,
+          popularity: row["popularity"].to_i,
+          duration_ms: row["duration_ms"].to_i,
+          image_url: row["image_url"],
+        )
+      end
+    end
+
+    attr_reader :title, :artist, :number_of_singers, :year, :popularity, :duration_ms, :image_url
+
+    def initialize(title:, artist:, number_of_singers:, year:, popularity:, duration_ms:, image_url:)
+      @title = title
+      @artist = artist
+      @number_of_singers = number_of_singers
+      @year = year
+      @popularity = popularity
+      @duration_ms = duration_ms
+      @image_url = image_url
+    end
+
+    def decade
+      (@year / 10).to_i * 10
+    end
+
+    def duration_seconds
+      if @duration_ms.nil?
+        debugger
+      end
+      (@duration_ms / 1000).to_i
+    end
+
+    def duration_minutes
+      (duration_seconds / 60).to_i
+    end
+  end
+
   def initialize(filepath)
     @filepath = filepath
-    @rows = CSV.read(filepath, headers: true)
+    @songs = CSV.read(filepath, headers: true).map do |row|
+      Song.from_row(row)
+    end
   end
 
   def to_json
-    @rows.map { |row| row.to_hash }.to_json
+    @songs.map { |row| row.to_hash }.to_json
   end
 
   def replace_contents(songs)
-    rows = []
+    songs = []
     songs.each do |song|
-      rows << [song[:title], song[:artist], song[:number_of_singers], song[:year], song[:popularity], song[:duration_ms], song[:image_url]]
+      songs << [song[:title], song[:artist], song[:number_of_singers], song[:year], song[:popularity], song[:duration_ms], song[:image_url]]
     end
 
     CSV.open(@filepath, "wb") do |csv|
       csv << ["title", "artist", "number_of_singers", "year", "popularity", "duration_ms", "image_url"]
-      rows.each do |row|
+      songs.each do |row|
         csv << row
       end
     end
+  end
+
+  def songs
+    @songs
   end
 end
